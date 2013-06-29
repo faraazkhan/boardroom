38900d1930c8ce896313704790a49f604ad98a6c
Michael Wynholds <mike@carbonfive.com>
2013-06-29 08:45:48 -0700

    Fixed a deploy bug

--- config/deploy/acceptance.rb
+++ config/deploy/acceptance.rb
@@ -1,5 +1,5 @@
 dir = '/var/apps/boardroom/acceptance'
-secrets = %w(TWITTER_SECRET GOOGLE_CLIENT_SECRET FACEBOOK_APP_SECRET).map { |s| "$(cat #{dir}/config/#{s})"}.join ' '
+secrets = %w(TWITTER_SECRET GOOGLE_CLIENT_SECRET FACEBOOK_APP_SECRET).map { |s| "#{s}=$(cat #{dir}/config/#{s})"}.join ' '
 
 set :branch, 'development'
 set :deploy_to, dir
