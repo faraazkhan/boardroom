8ac0bbf477ae53221b3b36f61007c1d6beee9a67
Michael Wynholds <mike@carbonfive.com>
2013-06-29 08:37:38 -0700

    Fixing deploy for acceptance

--- config/deploy/acceptance.rb
+++ config/deploy/acceptance.rb
@@ -1,4 +1,7 @@
+dir = '/var/apps/boardroom/acceptance'
+secrets = %w(TWITTER_SECRET GOOGLE_CLIENT_SECRET FACEBOOK_APP_SECRET).map { |s| "$(cat #{dir}/config/#{s})"}.join ' '
+
 set :branch, 'development'
-set :deploy_to, '/var/apps/boardroom/acceptance'
+set :deploy_to, dir
 set :node_env, 'acceptance'
-set :app_environment, 'PORT=1338'
+set :app_environment, "PORT=1338 #{secrets}"
