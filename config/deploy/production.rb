97c20728a8f534dc5a8cf3c6f1be97a0a5bfbb4e
Michael Wynholds <mike@carbonfive.com>
2013-06-29 08:58:41 -0700

    Deploy config for production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,7 @@
+dir = '/var/apps/boardroom/production'
+secrets = %w(TWITTER_SECRET GOOGLE_CLIENT_SECRET FACEBOOK_APP_SECRET).map { |s| "#{s}=$(cat #{dir}/config/#{s})"}.join ' '
+
 set :branch, 'master'
-set :deploy_to, '/var/apps/boardroom/production'
+set :deploy_to, dir
 set :node_env, 'production'
-set :app_environment, 'PORT=1337'
+set :app_environment, "PORT=1337 #{secrets}"
